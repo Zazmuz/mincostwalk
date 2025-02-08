cfbdb9b66e295aa8beb8f55d21baa3ec70f28753
Zazmuz <44049876+Zazmuz@users.noreply.github.com>
2025-02-08 10:48:56 +0100

    made problem

---
+++ input_validators/validator/validator.cpp
@@ -0,0 +1,12 @@
+#include "validator.h"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void run() {
+  int n = Int(1, 10000);
+  Endl();
+  SpacedInts(n, 1, 100);
+  Eof();
+}
+
---
+++ submissions/accepted/rasmus.py
@@ -0,0 +1,13 @@
+
+n = int(input())
+cost = [int(i) for i in input().split()]
+if n == 1:
+    print(cost[0])
+else:
+    dp = [0] * n
+    dp[0], dp[1] = cost[0], cost[1]
+
+    for i in range(2, n):
+        dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+
+    print(dp[-1])
